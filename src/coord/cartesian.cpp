d7eaa2fac3e500d3281710b72ea38b2cd2432661
Christopher J May <maychris@csrwks2021-0039.engin.umich.edu>
2026-06-12 15:24:01 -0400

    fixed issue where face areas were defined with dx arrays before they were initialized

--- src/coord/cartesian.cpp
+++ src/coord/cartesian.cpp
@@ -25,7 +25,6 @@ void CartesianImpl::reset() {
   x1v = register_buffer("x1v", 0.5 * (x1f.slice(0, 0, op->nc1()) +
                                       x1f.slice(0, 1, op->nc1() + 1)));
   dx1v = register_buffer("dx1v", torch::ones(op->nc1(), torch::kFloat64) * dx);
-  cfa1 = register_buffer("cfa1", dx3f.outer(dx2f).unsqueeze(2).expand({-1, -1, x1f.size(0)}));
 
   // dimension 2
   dx = (op->x2max() - op->x2min()) / op->nx2();
@@ -33,7 +32,6 @@ void CartesianImpl::reset() {
   x2v = register_buffer("x2v", 0.5 * (x2f.slice(0, 0, op->nc2()) +
                                       x2f.slice(0, 1, op->nc2() + 1)));
   dx2v = register_buffer("dx2v", torch::ones(op->nc2(), torch::kFloat64) * dx);
-  cfa2 = register_buffer("cfa2", dx3f.outer(dx1f).unsqueeze(1).expand({-1, x2f.size(0), -1}));
 
   // dimension 3
   dx = (op->x3max() - op->x3min()) / op->nx3();
@@ -41,8 +39,10 @@ void CartesianImpl::reset() {
   x3v = register_buffer("x3v", 0.5 * (x3f.slice(0, 0, op->nc3()) +
                                       x3f.slice(0, 1, op->nc3() + 1)));
   dx3v = register_buffer("dx3v", torch::ones(op->nc3(), torch::kFloat64) * dx);
-  cfa3 = register_buffer("cfa3", dx2f.outer(dx1f).unsqueeze(0).expand({x3f.size(0), -1, -1}));
 
+  cfa1 = register_buffer("cfa1", dx3f.outer(dx2f).unsqueeze(2).expand({-1, -1, x1f.size(0)}));
+  cfa2 = register_buffer("cfa2", dx3f.outer(dx1f).unsqueeze(1).expand({-1, x2f.size(0), -1}));
+  cfa3 = register_buffer("cfa3", dx2f.outer(dx1f).unsqueeze(0).expand({x3f.size(0), -1, -1}));
   cvol = register_buffer("cvol", torch::einsum("km,mji->kji", {dx3f.unsqueeze(1), dx2f.outer(dx1f).unsqueeze(0)}));
 
   // register buffers defined in the base class
